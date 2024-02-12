f7ac11b272010745ee63b255db0c655fbe2098a9
kazmiali123 <kazmiali123@gmail.com>
2024-02-12 15:26:13 -0500

    contract page styling

--- client/src/pages/ContractPage.jsx
+++ client/src/pages/ContractPage.jsx
@@ -10,7 +10,8 @@ import {
     Badge,
 } from "react-bootstrap";
 import { useRentalContext } from "../utils/GlobalContext";
-
+import logo from '../assets/site-footer2-logo.png'
+import '../contract.css';
 
 
 
@@ -22,43 +23,89 @@ export default function ContractPage() {
 
 
     return (
-        <Container fluid>
-            {!shoppingCart ? (
-                <div>... Loading</div>
-            ) : (
-                <div>
-                    <h2>Contract Page</h2>
-                    <Row>
-                        <Col sm={8}>{shoppingCart.userName}</Col>
-                        <Col sm={8}>{shoppingCart.bikeInfo}</Col>
-                        <Col sm={8}>{shoppingCart.rentalPerDay}</Col>
-                        <Col sm={8}>{shoppingCart.insurancePerDay}</Col>
-                        <Col sm={8}>{shoppingCart.duration}</Col>
-                        <Col sm={8}>{shoppingCart.rentalPriceSub}</Col>
-                        <Col sm={8}>{shoppingCart.rentalPriceTotal}</Col>
+        <Container >
+            <div className="myOutlet">
+                <div className="purchaseCard">
+                    <Container fluid>
+                        {!shoppingCart ? (
+                            <div>... Loading</div>
+                        ) : (
+                            <div>
+                                <h2>Contract</h2>
+                                <br />
+                                <Row>
+                                    <Col sm={6}>Username</Col>
+                                    <Col xs={6}><strong>{shoppingCart.userName}</strong></Col>
+                                </Row>
+                                <br />
+                                <Row>
+                                    <Col sm={6}>Bike Description</Col>
+                                    <Col xs={6}><strong>{shoppingCart.bikeInfo}</strong></Col>
+                                </Row>
+                                <br />
+                                <Row>
+                                    <Col sm={6}>Rental (per day)</Col>
+                                    <Col xs={6}><strong>{"$" + shoppingCart.rentalPerDay}</strong></Col>
+                                </Row>
+                                <br />
+                                <Row>
+                                    <Col sm={6}>Insurance (per day)</Col>
+                                    <Col xs={6}><strong>{"$" + shoppingCart.insurancePerDay}</strong></Col>
+                                </Row>
+                                <br />
+                                <Row>
+                                    <Col sm={6}>Term (days)</Col>
+                                    <Col xs={6}><strong>{shoppingCart.duration}</strong></Col>
+                                </Row>
+                                <br />
+                                <Row>
+                                    <Col sm={6}>Sub total</Col>
+                                    <Col xs={6}><strong>{"$" + shoppingCart.rentalPriceSub}</strong></Col>
+                                </Row>
+                                <br />
+                                <Row>
+                                    <Col sm={6}>Tax(HST)</Col>
+                                    <Col xs={6}><strong>{"$" + ((shoppingCart.rentalPriceSub * 0.13).toFixed(2))}</strong></Col>
+                                </Row>
+                                <br />
+                                <Row>
+                                    <Col sm={6}><h3>Total: </h3></Col>
+                                    <Col xs={6}><h3>{"$" + shoppingCart.rentalPriceTotal}</h3></Col>
+                                </Row>
+                                <br />
 
-                    </Row>
-                    <Button
-                        id="rentButton"
-                        variant="danger"
-                        as={Link}
-                        to="/contract"
-                    // onClick={handleRentContract}
-                    >
-                        Cancel
-                    </Button>
-                    <Button
-                        id="rentButton"
-                        variant="primary"
-                        as={Link}
-                        to="/contract"
-                    // onClick={handleRentContract}
-                    >
-                        Checkout to Rent
-                    </Button>
+                                <Row className="buttonGroup">
+                                    <Col >
+                                        <Button
+                                            id="checkoutBtn"
+                                            variant="success"
+                                            size="lg"
+                                            as={Link}
+                                            to="/contract"
+                                        // onClick={handleRentContract}
+                                        >
+                                            Checkout to Rent
+                                            <Image className='checkoutLogo' src={logo} width="70" height="50" />
+                                        </Button>
+                                    </Col>
+                                    <br />
+                                    <Col >
+                                        <Button
+                                            id="cancelBtn"
+                                            variant="secondary"
+                                            as={Link}
+                                            to="/contract"
+                                        // onClick={handleRentContract}
+                                        >
+                                            Cancel
+                                        </Button>
+                                    </Col>
+                                </Row>
+                            </div>
+                        )}
+                    </Container>
                 </div>
-
-            )}
+            </div>
         </Container>
     );
 }
