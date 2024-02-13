33339df0951e3ee70ec18eb3a16944f74a9f2c89
Indra Levi Manahan <indra.levi.manahan@gmail.com>
2024-02-13 09:38:12 -0500

    config

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -5,7 +5,7 @@ const { calculateInsuranceQuote } = require('../models/Insurance')
 
 // Connect to MongoDB
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bikeDB', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://MERNSearch:bEwdIHNJp95SAX2m@cluster0.wkoffnc.mongodb.net/bikeDB?retryWrites=true&w=majority', {
 
   useNewUrlParser: true,
   useUnifiedTopology: true,
