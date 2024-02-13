be9f90d1eed272a0a9014ae8532e05377261861c
Indra Levi Manahan <indra.levi.manahan@gmail.com>
2024-02-13 11:01:59 -0500

    Final polishing

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-console.log(process.env.MONGODB_URI);
+
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bikeDB');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://MERNSearch:bEwdIHNJp95SAX2m@cluster0.wkoffnc.mongodb.net/bikeDB?retryWrites=true&w=majority')
 
